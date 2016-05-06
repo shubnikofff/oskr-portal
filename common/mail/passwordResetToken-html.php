f33a87c38dc5050b591ee320cb0bdfbd07f5793b
Shubnikov Alexey <shubnikov.av@gmail.com>
2016-05-06 11:19:35 +0300

    Add username to reset password mail message

--- common/mail/passwordResetToken-html.php
+++ common/mail/passwordResetToken-html.php
@@ -10,6 +10,9 @@ $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 't
 
     <p>Здравствуйте, <?= Html::encode($user->fullName) ?>.</p>
 
+    <p>Вы зарегистрированы на <?= Html::encode(Yii::$app->name) ?> с именем(логином)
+        <b><?= Html::encode($user->username) ?></b></p>
+
     <p>Для сброса пароля проследуйте по следующей ссылке:</p>
 
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
--- common/mail/passwordResetToken-text.php
+++ common/mail/passwordResetToken-text.php
@@ -7,6 +7,8 @@ $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 't
 ?>
 Здравствуйте, <?= $user->fullName ?>.
 
+Вы зарегистрированы на '<?= Yii::$app->name ?>' с именем(логином) '<?= $user->username ?>'
+
 Для сброса пароля проследуйте по следующей ссылке:
 
 <?= $resetLink ?>
