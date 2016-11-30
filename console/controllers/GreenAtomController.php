17cbc1bf647d297087a72a2837b3a48f494a232f
Shubnikov Alexey <shubnikov.av@gmail.com>
2016-11-30 15:41:33 +0300

    Add GreenAtom notify command

---
+++ console/controllers/GreenAtomController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * oskr-portal
+ * Created: 30.11.16 9:31
+ * @copyright Copyright (c) 2016 OSKR NIAEP
+ */
+
+namespace console\controllers;
+
+use frontend\components\services\FutureMeetingGreenAtomNotifier;
+use frontend\models\vks\Request;
+use yii\console\Controller;
+
+/**
+ * @author Shubnikov Alexey <a.shubnikov@niaep.ru>
+ *
+ * GreenAtomController
+ */
+class GreenAtomController extends Controller
+{
+    public function actionPeriodicNotification($interval)
+    {
+        $beginTime = intval(date('G')) * 60 + intval(date('i'));
+        $set = Request::find()->where([
+            'date' => new \MongoDate(gmmktime(0, 0, 0)),
+            'beginTime' => [
+                '$gte' => $beginTime,
+                '$lt' => $beginTime + $interval
+            ],
+            'status' => Request::STATUS_APPROVE
+        ])->all();
+
+        self::notify($set);
+    }
+
+    public function actionEveningNotification()
+    {
+        $beginTime = 8 * 60;
+        $tomorrowDate = new \MongoDate(strtotime("+1 day", gmmktime(0, 0, 0)));
+        $set = Request::find()->where([
+            'date' => $tomorrowDate,
+            'beginTime' => [
+                '$gte' => $beginTime,
+                '$lt' => $beginTime + 60,
+            ],
+            'status' => Request::STATUS_APPROVE
+        ])->all();
+
+        self::notify($set);
+    }
+
+    /**
+     * @param $requestSet Request[]
+     */
+    protected static function notify($requestSet)
+    {
+        foreach ($requestSet as $request) {
+            FutureMeetingGreenAtomNotifier::sendMail($request);
+        }
+    }
+}
